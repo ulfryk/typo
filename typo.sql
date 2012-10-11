213e1fe9b2b4ec3a76b38b736a9a42af9b917dd0
ulfryk <jakub.strojewski@gmail.com>
2012-10-11 09:48:51 +0200

    sql fix

--- typo.sql
+++ typo.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 10, 2012 at 02:36 PM
+-- Generation Time: Oct 11, 2012 at 07:45 AM
 -- Server version: 5.5.23
 -- PHP Version: 5.3.15
 
@@ -27,8 +27,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `letters` (
-  `id` int(11) NOT NULL,
-  `packs` varchar(50) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pack` varchar(30) NOT NULL,
   `row` varchar(15) NOT NULL,
   `range` varchar(15) NOT NULL,
   PRIMARY KEY (`id`)
@@ -38,12 +38,43 @@ CREATE TABLE IF NOT EXISTS `letters` (
 -- Dumping data for table `letters`
 --
 
-INSERT INTO `letters` (`id`, `packs`, `row`, `range`) VALUES
+INSERT INTO `letters` (`id`, `pack`, `row`, `range`) VALUES
 (0, 'asdf', 'home', 'left'),
 (1, 'jkl;', 'home', 'right'),
 (2, 'asdfg', 'home', 'leftex'),
 (3, 'hjkl;', 'home', 'rightex'),
-(4, 'asdfghjkl;', 'home', 'both');
+(4, 'asdfghjkl;', 'home', 'both'),
+(5, 'zxcv', 'bottom', 'left'),
+(6, 'zxcvb', 'bottom', 'leftex'),
+(7, 'm,./', 'bottom', 'right'),
+(8, 'nm,./', 'bottom', 'rightex'),
+(9, 'zxcvbnm,./', 'bottom', 'both'),
+(10, 'qwer', 'top', 'left'),
+(11, 'qwert', 'top', 'leftex'),
+(12, 'uiop', 'top', 'right'),
+(13, 'yuiop', 'top', 'rightex'),
+(14, 'qwertyuiop', 'top', 'both');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `words`
+--
+
+CREATE TABLE IF NOT EXISTS `words` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `word` varchar(30) NOT NULL,
+  `row` varchar(15) NOT NULL,
+  `range` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `words`
+--
+
+INSERT INTO `words` (`id`, `word`, `row`, `range`) VALUES
+(1, 'fagas', 'home', 'left');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
